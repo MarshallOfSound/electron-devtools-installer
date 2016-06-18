e2ae77bfd6c982db19bc3d730f76038bea7b361b
Samuel Attard <samuel.r.attard@gmail.com>
2016-06-18 15:00:27 +1000

    https module is dodgy, use the request module

--- src/downloadChromeExtension.js
+++ src/downloadChromeExtension.js
@@ -2,6 +2,7 @@ import electron, { remote } from 'electron';
 import fs from 'fs';
 import { https } from 'follow-redirects';
 import path from 'path';
+import request from 'request';
 import rimraf from 'rimraf';
 import unzip from 'cross-unzip';
 
@@ -19,19 +20,19 @@ const downloadChromeExtension = (chromeStoreID, forceDownload) => {
       }
       const fileURL = `https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D${chromeStoreID}%26uc&prodversion=32`; // eslint-disable-line
       const download = fs.createWriteStream(path.resolve(`${extensionFolder}.crx`));
-      https.get(fileURL, (response) => {
-        response
-          .pipe(download)
-          .on('close', () => {
-            unzip(path.resolve(`${extensionFolder}.crx`), extensionFolder, (err) => {
-              if (err && !fs.existsSync(path.resolve(extensionFolder, 'manifest.json'))) {
-                return reject(err);
-              }
-              resolve(extensionFolder);
-            });
-          });
-      }).on('error', (err) => {
-        reject(err);
+      request({
+        url: fileURL,
+        followAllRedirects: true,
+      })
+      .pipe(download)
+      .on('error', (err) => reject(err))
+      .on('close', () => {
+        unzip(path.resolve(`${extensionFolder}.crx`), extensionFolder, (err) => {
+          if (err && !fs.existsSync(path.resolve(extensionFolder, 'manifest.json'))) {
+            return reject(err);
+          }
+          resolve(extensionFolder);
+        });
       });
     } else {
       resolve(extensionFolder);
