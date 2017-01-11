af4713414e81095161076d1ad67a9b3c6dc03f7f
Niels Vleeming <niels.vleeming@gmail.com>
2017-01-11 05:29:04 +0100

    Fix first run error caused by mkdir in getPath (#32)
    The mkdir in getPath gets executed on import whenever this library is used. This causes an error to be thrown when the electron app that uses it is freshly installed, and the electron userData directory is not yet created.
    
    This change moves the mkdir call to the first place the existence of the extensionsStore is actually required.
    
    Fixes #29

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,7 @@
 import electron, { remote } from 'electron';
-import fs from 'fs';
 import path from 'path';
 
 export const getPath = () => {
   const savePath = (remote || electron).app.getPath('userData');
-  const extensionsStore = path.resolve(`${savePath}/extensions`);
-  if (!fs.existsSync(extensionsStore)) {
-    fs.mkdirSync(extensionsStore);
-  }
-  return extensionsStore;
+  return path.resolve(`${savePath}/extensions`);
 };
