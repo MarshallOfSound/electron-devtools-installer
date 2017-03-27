e44a958857df17e6b249055b4ff5c27b529a36fb
Omri Klinger <omri.klinger@gmail.com>
2017-03-27 09:39:02 -0400

    Update index.js
    Added APOLLO_DEVTOOLS chrome extension (Apollo Client Developer Tools - http://dev.apollodata.com/)
    Re-Order extension names A->Z.

--- src/index.js
+++ src/index.js
@@ -56,39 +56,47 @@ const install = (extensionReference, forceDownload = false) => {
 };
 
 export default install;
-export const EMBER_INSPECTOR = {
-  id: 'bmdblncegkenkacieihfhpjfppoconhi',
+
+export const ANGULARJS_BATARANG = {
+  id: 'ighdmehidhipcmcojjgiloacoafjmpfk',
   electron: '^1.2.1',
 };
-export const REACT_DEVELOPER_TOOLS = {
-  id: 'fmkadmapgofadopljbjfkapdkoienihi',
+export const APOLLO_DEVTOOLS = {
+  id: 'jdkknkkbebbapilgoeccciglkfbmbnfm',
   electron: '^1.2.1',
 };
 export const BACKBONE_DEBUGGER = {
   id: 'bhljhndlimiafopmmhjlgfpnnchjjbhd',
   electron: '^1.2.1',
 };
+export const CYCLEJS_DEVTOOL = {
+  id: 'dfgplfmhhmdekalbpejekgfegkonjpfp',
+  electron: '^1.2.1',
+};
+export const EMBER_INSPECTOR = {
+  id: 'bmdblncegkenkacieihfhpjfppoconhi',
+  electron: '^1.2.1',
+};
 export const JQUERY_DEBUGGER = {
   id: 'dbhhnnnpaeobfddmlalhnehgclcmjimi',
   electron: '^1.2.1',
 };
-export const ANGULARJS_BATARANG = {
-  id: 'ighdmehidhipcmcojjgiloacoafjmpfk',
-  electron: '^1.2.1',
-};
-export const VUEJS_DEVTOOLS = {
-  id: 'nhdogjmejiglipccpnnnanhbledajbpd',
-  electron: '^1.2.1',
-};
-export const REDUX_DEVTOOLS = {
-  id: 'lmhkpmbekcpmknklioeibfkpmmfibljd',
+export const REACT_DEVELOPER_TOOLS = {
+  id: 'fmkadmapgofadopljbjfkapdkoienihi',
   electron: '^1.2.1',
 };
 export const REACT_PERF = {
   id: 'hacmcodfllhbnekmghgdlplbdnahmhmm',
   electron: '^1.2.6',
 };
-export const CYCLEJS_DEVTOOL = {
-  id: 'dfgplfmhhmdekalbpejekgfegkonjpfp',
+export const REDUX_DEVTOOLS = {
+  id: 'lmhkpmbekcpmknklioeibfkpmmfibljd',
   electron: '^1.2.1',
 };
+export const VUEJS_DEVTOOLS = {
+  id: 'nhdogjmejiglipccpnnnanhbledajbpd',
+  electron: '^1.2.1',
+};
+
+
+
