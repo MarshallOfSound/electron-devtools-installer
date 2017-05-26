6354a4016cb2653f056658504aa9260dc88b0036
Jhen <developer@jhen.me>
2017-05-27 01:01:57 +0800

    needUpdate: Remove reject statement

--- src/needUpdate.js
+++ src/needUpdate.js
@@ -1,23 +1,19 @@
 import { parseString } from 'xml2js';
 import { fetchData } from './utils';
 
-export default (chromeStoreID, currentVersion) => new Promise((resolve, reject) =>
+export default (chromeStoreID, currentVersion) => new Promise(resolve =>
   fetchData(
     `https://clients2.google.com/service/update2/crx?x=id%3D${chromeStoreID}%26uc&prodversion=32`,
   ).then((res) => {
-    if (res.statusCode === 200) {
-      parseString(res.body, (err, result) => {
-        const app = result.gupdate.app[0].$;
-        if (app.status !== 'ok') return resolve(false);
+    parseString(res.body, (err, result) => {
+      const app = result.gupdate.app[0].$;
+      if (app.status !== 'ok') return resolve(false);
 
-        const { status, version: newestVersion } = result.gupdate.app[0].updatecheck[0].$;
-        if (status !== 'ok') return resolve(false);
+      const { status, version: newestVersion } = result.gupdate.app[0].updatecheck[0].$;
+      if (status !== 'ok') return resolve(false);
 
-        if (newestVersion !== currentVersion) return resolve(true);
-        return resolve(false);
-      });
-    } else {
-      reject(`Failed to check current version of ${chromeStoreID}.`);
-    }
+      if (newestVersion !== currentVersion) return resolve(true);
+      return resolve(false);
+    });
   }),
 );
