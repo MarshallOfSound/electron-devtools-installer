cae5e6330d949bca005e99a93bda47f9e0927761
nashaofu <diaocheng@outlook.com>
2018-02-22 22:45:55 +0800

    fix unzip error on windows
    remove 7zip cross-unzip
    add unzip-crx

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ const install = (extensionReference, forceDownload = false) => {
   const extensionInstalled = extensionName &&
     BrowserWindow.getDevToolsExtensions &&
     BrowserWindow.getDevToolsExtensions()[extensionName];
+    console.log(extensionInstalled, BrowserWindow.getDevToolsExtensions())
   if (!forceDownload && extensionInstalled) {
     return Promise.resolve(IDMap[chromeStoreID]);
   }
@@ -49,6 +50,7 @@ const install = (extensionReference, forceDownload = false) => {
       if (extensionInstalled) {
         BrowserWindow.removeDevToolsExtension(extensionName);
       }
+      console.log(extensionFolder)
       const name = BrowserWindow.addDevToolsExtension(extensionFolder); // eslint-disable-line
       fs.writeFileSync(
         IDMapPath,
