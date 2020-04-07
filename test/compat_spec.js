3e11854abf1dedda5a911564e3238d3ff804ed08
Samuel Attard <samuel.r.attard@gmail.com>
2020-04-06 17:22:30 -0700

    build: add circleci config

--- test/compat_spec.js
+++ test/compat_spec.js
@@ -12,7 +12,7 @@ describe('Extension Compatability Checker', () => {
   let currentVersion;
 
   beforeEach(() => {
-    currentVersion = process.versions.electron;
+    currentVersion = Object.getOwnPropertyDescriptor(process.versions, 'electron');
   });
 
   describe('when using a compatable version of electron', () => {
@@ -22,26 +22,29 @@ describe('Extension Compatability Checker', () => {
 
   describe('when using a major bump compatable version of electron', () => {
     it('should resolve the promise', () => {
-      process.versions.electron = '3.0.0';
+      Object.defineProperty(process.versions, 'electron', { value: '3.0.0', configurable: true });
       return installExtension(REACT_DEVELOPER_TOOLS).should.be.fulfilled;
     });
   });
 
   describe('when using an incompatable version of electron', () => {
     it('should reject the promise', () => {
-      process.versions.electron = '0.37.2';
+      Object.defineProperty(process.versions, 'electron', { value: '0.37.2', configurable: true });
       return installExtension(REACT_DEVELOPER_TOOLS).should.be.rejected;
     });
   });
 
   describe('when using a compatable beta version of electron', () => {
     it('should resolve the promise', () => {
-      process.versions.electron = '1.8.2-beta.3';
+      Object.defineProperty(process.versions, 'electron', {
+        value: '1.8.2-beta.3',
+        configurable: true,
+      });
       return installExtension(REACT_DEVELOPER_TOOLS).should.be.fulfilled;
     });
   });
 
   afterEach(() => {
-    process.versions.electron = currentVersion;
+    Object.defineProperty(process.versions, 'electron', currentVersion);
   });
 });
