fe2eeb61ff2e884fba65ed7bfffba92451655f1e
Matthias Schiffer <mschiffer@universe-factory.net>
2020-07-11 00:03:47 +0200

    Add CRX3 support
    Google servers recently started sending empty replies when
    acceptformat=crx2,crx3 is not passed. Add the option to the URL query
    and switch to unzip-crx-3, which supports both CRX v2 and v3.
    
    Closes: #139

--- src/downloadChromeExtension.js
+++ src/downloadChromeExtension.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import rimraf from 'rimraf';
-import unzip from 'unzip-crx';
+import unzip from 'unzip-crx-3';
 
 import { getPath, downloadFile, changePermissions } from './utils';
 
@@ -16,7 +16,7 @@ const downloadChromeExtension = (chromeStoreID, forceDownload, attempts = 5) => 
       if (fs.existsSync(extensionFolder)) {
         rimraf.sync(extensionFolder);
       }
-      const fileURL = `https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D${chromeStoreID}%26uc&prodversion=32`; // eslint-disable-line
+      const fileURL = `https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&x=id%3D${chromeStoreID}%26uc&prodversion=32`; // eslint-disable-line
       const filePath = path.resolve(`${extensionFolder}.crx`);
       downloadFile(fileURL, filePath)
         .then(() => {
